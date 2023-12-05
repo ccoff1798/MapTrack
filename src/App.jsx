2624fbfc97da677f45ce6a88c19c132870d53122
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-12-04 21:54:06 -0700

    added navtabs, changed actions titles for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import NavTabs from './components/NavTabs.jsx';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -11,6 +12,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div className="flex-column justify-center align-center min-100-vh bg-primary">
+      <NavTabs />
         <Outlet />
       </div>
     </ApolloProvider>
